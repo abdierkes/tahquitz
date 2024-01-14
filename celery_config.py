47d9722f35f446722407e55ef443527dd75bbedc
abdierkes <abdierkes@gmail.com>
2024-01-14 15:28:58 -0800

    added a new test for worker

--- celery_config.py
+++ celery_config.py
@@ -3,7 +3,7 @@ from celery.schedules import crontab
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
-timezone = 'Pacific/Central'
+CELERY_TIMEZONE = 'Pacific/Central'
 
 CELERYBEAT_SCHEDULE = {
     'parse_weather_daily': {
